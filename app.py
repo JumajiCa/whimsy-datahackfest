bdf6155a285709db840cbff72218c888d856b5b2
JumajiCa <AmenYiHonduras@Gmail.Com>
2025-07-26 21:46:51 -0700

    Minor tweaks.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 
 from flask import Flask 
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world() -> str: 
-    return "<p style=\"text-align: center; font-size: 100px\">Flask Text Test!😭</p>"
+    web_text:str = ""
+    if "FLASK_ENV" and "FLASK_APP" in os.environ:
+        web_text= f"\'FLASK_ENV\': {os.environ["FLASK_ENV"]} | \'FLASK_APP\': {os.environ["FLASK_APP"]}"
+    if not web_text: 
+        return f"<p style=\"text-align: center; font-size: 100px\">😭</p>"
+    else: 
+        return f"<p style=\"text-align: center; font-size: 100px\"> {web_text} 😭</p>"
 
 if __name__ == "__main__": 
     app.run(debug=True)
